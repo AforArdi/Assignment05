cf0e1cea2fa43767b805b264bb81af3684f091fa
AforArdi <binsbro36@gmail.com>
2026-03-08 01:02:44 +0600

    modal functional, working on some undefined issues

--- scripts/index.js
+++ scripts/index.js
@@ -69,6 +69,55 @@ btnSearch.addEventListener('click', ()=>{
             issuesCount(allIssues);
         })
 })
+// modal loading data and showing data related functionaltiy
+const loadForModalClick=async(id)=>{
+    const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`)
+    const data = await res.json();
+    displayForModalClick(data);
+}
+const displayForModalClick=(issue)=>{
+    // console.log(issue.data);
+    const modalDetailsContainer = document.getElementById('modal-details-container');
+    const labels = issue.data.labels.map(label => {
+        return `<div class="badge badge-warning text-[12px] font-medium whitespace-nowrap">${label}</div>`;
+    }).join('');
+    
+    modalDetailsContainer.innerHTML = `
+        <h3 class="text-lg font-bold">${issue.title}</h3>
+            <div class="flex gap-2">
+                <div class="badge badge-soft badge-success text-[12px] font-medium">${issue.status}</div>
+                &bull;
+                <p>Opened by ${issue.author}</p>
+                &bull;
+                <p>${issue.createdAt}</p>
+            </div>
+            <div class="flex gap-2">
+                <div class="flex gap-2">
+                    ${labels}
+                </div>
+            </div>
+            <p class="line-clamp-3">${issue.description}</p>
+            <div class=" bg-[#F8FAFC] mx-auto p-4 rounded-md">
+                <div class="flex justify-between items-center">
+                    <div>
+                        <h2>Assignee:</h2>
+                        <p>${issue.assignee}</p>
+                    </div>
+                    <div>
+                        <h2>Priority:</h2>
+                        <div class="badge badge-soft badge-warning text-[12px] font-medium">${issue.priority}</div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-action">
+                <form method="dialog">
+                    <!-- if there is a button in form, it will close the modal -->
+                    <button class="btn btn-primary">Close</button>
+                </form>
+            </div>
+    `;
+    document.getElementById('my_modal_1').showModal();
+}
 
 
 // loading all isues from API
@@ -90,7 +139,7 @@ const displayIssues=(issues)=>{
         }).join('');
 
         issueCard.innerHTML = `
-            <div class="card bg-base-100 w-72 h-70 shadow-sm">
+            <div onclick="loadForModalClick(${issue.id})" class="card bg-base-100 w-72 h-70 shadow-sm cursor-pointer">
                 <div class="card-body">
                     <div class="badge badge-soft badge-warning text-[12px] font-medium">${issue.priority}</div>
                     <h2 class="font-semibold text-[14px]">${issue.title}</h2>
